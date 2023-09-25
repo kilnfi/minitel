910954bb9332d582c0c0874b9a93d37a414bded9
LeTamanoir <martin.saldinger@gmail.com>
2023-09-25 23:38:27 +0200

    parser functions nits

--- src/lib/parseAdaTx.ts
+++ src/lib/parseAdaTx.ts
@@ -1,10 +1,8 @@
-import { prettyPrintJson } from "pretty-print-json";
-import { Transaction } from "@emurgo/cardano-serialization-lib-nodejs";
+import { prettyPrintJson } from 'pretty-print-json';
+import { Transaction } from '@emurgo/cardano-serialization-lib-nodejs';
 
 export const parseAdaTx = async (txRaw: string): Promise<string> => {
-  const tx = Transaction.from_hex(txRaw);
+	const tx = Transaction.from_hex(txRaw);
 
-  return prettyPrintJson.toHtml(tx.to_js_value(), {
-    quoteKeys: true,
-  })
-}
+	return prettyPrintJson.toHtml(tx.to_js_value(), { quoteKeys: true });
+};
--- src/lib/parseAtomTx.ts
+++ src/lib/parseAtomTx.ts
@@ -1,11 +1,9 @@
-import { prettyPrintJson } from "pretty-print-json";
-import { Buffer } from "buffer";
-import { decodeTxRaw } from "@cosmjs/proto-signing";
+import { prettyPrintJson } from 'pretty-print-json';
+import { Buffer } from 'buffer';
+import { decodeTxRaw } from '@cosmjs/proto-signing';
 
 export const parseAtomTx = async (txRaw: string): Promise<string> => {
-  const tx = decodeTxRaw(Uint8Array.from(Buffer.from(txRaw, "hex")))
+	const tx = decodeTxRaw(Uint8Array.from(Buffer.from(txRaw, 'hex')));
 
-  return prettyPrintJson.toHtml(tx, {
-    quoteKeys: true,
-  })
-}
+	return prettyPrintJson.toHtml(tx, { quoteKeys: true });
+};
