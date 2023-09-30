9cd1fb46c14402a298d36b512d3dc001c2c92d7e
Arno Simon <arnos29@gmail.com>
2023-09-30 17:08:35 +0200

    update favicon

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
   <div class="flex justify-between items-center">
     <div>
       <a href="/" class="flex items-center rounded-md bg-black dark:border text-white">
-        <img src="/favicon.png" alt="logo" width="32" height="32" class="rounded-md" />
+        <img src="/favicon.jpg" alt="logo" width="32" height="32" class="rounded-md" />
         <span class="px-1.5 py-0.5">minitel.xyz</span>
       </a>
     </div>
