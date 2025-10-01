20c201f6f7eccc321d1f5844730a01ec12697449
menefrego15 <ilies.zareb@kiln.fi>
2025-10-01 16:20:07 +0200

    fix

--- apps/ethereum/vite.config.ts
+++ apps/ethereum/vite.config.ts
@@ -10,11 +10,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@protocols/ui': path.resolve(__dirname, '../../packages/ui/src'),
       '@protocols/shared': path.resolve(__dirname, '../../packages/shared/src'),
-      '@shared': path.resolve(__dirname, '../../packages/shared/src'),
-      '#/lib': path.resolve(__dirname, '../../packages/ui/src/lib'),
-      '#/components': path.resolve(__dirname, '../../packages/ui/src/components'),
-      '#/ui': path.resolve(__dirname, '../../packages/ui/src/components/ui'),
-      '#/hooks': path.resolve(__dirname, '../../packages/ui/src/hooks'),
     },
   },
 });
