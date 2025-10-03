96b170a147a1266cc683d36e835ed975e4e33da5
Ilies Zareb <73179254+menefrego15@users.noreply.github.com>
2025-10-03 11:51:44 +0200

    fix remove unused lib from vite config (#23)

--- apps/ton/vite.config.ts
+++ apps/ton/vite.config.ts
@@ -3,13 +3,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    wasm(),
     nodePolyfills({
       include: ['buffer'],
       globals: {
